88c3a5c6056c5904fd2da4ebd923d136a7b1a762
2nayeon <leenayeon0915@gmail.com>
2025-11-25 23:52:58 +0900

    Dump file v0.1

---
+++ sql/gardenlogdb_garden.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gardenlogdb
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `garden`
+--
+
+DROP TABLE IF EXISTS `garden`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `garden` (
+  `gardenid` int NOT NULL AUTO_INCREMENT,
+  `userid` varchar(20) NOT NULL,
+  `gardenname` varchar(100) NOT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `area` int DEFAULT NULL,
+  `crop_count` int DEFAULT '0',
+  `start_date` date DEFAULT NULL,
+  `expected_harvest_date` date DEFAULT NULL,
+  PRIMARY KEY (`gardenid`),
+  KEY `userid_idx` (`userid`),
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `garden`
+--
+
+LOCK TABLES `garden` WRITE;
+/*!40000 ALTER TABLE `garden` DISABLE KEYS */;
+INSERT INTO `garden` VALUES (1,'leenayeon','나연이네 베란다','서울특별시 구로구 개봉동',2,3,'2025-03-15',NULL),(2,'leenayeon','주말농장','서울특별시 구로구 개봉동',8,4,'2025-04-10',NULL),(3,'leeseyeon','세연이네 베란다','경기도 광명시 새터로',2,3,'2025-03-15',NULL),(4,'leeseyeon','주말농장','경기도 광명시 새터로',8,4,'2025-04-10',NULL),(5,'chohyein','혜인이네 베란다','서울시 서대문구 통일로',2,3,'2025-03-15',NULL),(6,'chohyein','주말농장','서울시 서대문구 통일로',8,4,'2025-04-10',NULL);
+/*!40000 ALTER TABLE `garden` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-25 22:54:12
