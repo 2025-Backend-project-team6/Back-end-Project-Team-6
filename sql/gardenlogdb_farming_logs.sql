c361c481d1f0c964d51460baac4112fae5128e35
chiyyy <hyein031114@gmail.com>
2025-12-03 12:40:55 +0900

    Dump file v0.8

---
+++ sql/gardenlogdb_farming_logs.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gardenlogdb
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `farming_logs`
+--
+
+DROP TABLE IF EXISTS `farming_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `farming_logs` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `userid` varchar(20) NOT NULL,
+  `my_crop_id` int DEFAULT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text,
+  `weather` varchar(20) DEFAULT NULL,
+  `log_date` date DEFAULT (curdate()),
+  `log_img` varchar(255) DEFAULT NULL,
+  `reg_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`),
+  KEY `userid` (`userid`),
+  KEY `my_crop_id` (`my_crop_id`),
+  CONSTRAINT `farming_logs_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
+  CONSTRAINT `farming_logs_ibfk_2` FOREIGN KEY (`my_crop_id`) REFERENCES `my_crop` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `farming_logs`
+--
+
+LOCK TABLES `farming_logs` WRITE;
+/*!40000 ALTER TABLE `farming_logs` DISABLE KEYS */;
+INSERT INTO `farming_logs` VALUES (1,'testUser',5,'방울이 첫 꽃','노란 꽃이 피었다!','맑음','2025-12-02',NULL,'2025-12-02 04:23:44');
+/*!40000 ALTER TABLE `farming_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-03 12:24:53
