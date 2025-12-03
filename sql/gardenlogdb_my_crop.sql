996e3f8a32bc5575668eff19a77346a6695dc9af
2nayeon <leenayeon0915@gmail.com>
2025-12-03 12:23:21 +0900

    Dump file v0.6

--- sql/gardenlogdb_my_crop.sql
+++ sql/gardenlogdb_my_crop.sql
@@ -27,7 +27,7 @@ CREATE TABLE `my_crop` (
   `userid` varchar(20) NOT NULL,
   `gardenid` int NOT NULL,
   `cropid` int NOT NULL,
-  `nickname` varchar(50) DEFAULT NULL,
+  `nickname` varchar(50) NOT NULL,
   `planted_date` date NOT NULL,
   `water_count` int NOT NULL DEFAULT '0',
   `last_watered_at` datetime DEFAULT NULL,
@@ -35,13 +35,15 @@ CREATE TABLE `my_crop` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `userid` (`userid`),
-  KEY `my_crop_ibfk_2` (`cropid`),
+  KEY `fk_my_crop_user` (`userid`),
   KEY `fk_my_crop_garden` (`gardenid`),
+  KEY `fk_my_crop_cropinfo` (`cropid`),
+  CONSTRAINT `fk_my_crop_cropinfo` FOREIGN KEY (`cropid`) REFERENCES `crop_info` (`cropid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_my_crop_garden` FOREIGN KEY (`gardenid`) REFERENCES `garden` (`gardenid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_my_crop_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `my_crop_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE RESTRICT,
   CONSTRAINT `my_crop_ibfk_2` FOREIGN KEY (`cropid`) REFERENCES `crop_info` (`cropid`) ON DELETE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +52,7 @@ CREATE TABLE `my_crop` (
 
 LOCK TABLES `my_crop` WRITE;
 /*!40000 ALTER TABLE `my_crop` DISABLE KEYS */;
-INSERT INTO `my_crop` VALUES (3,'leenayeon',1,6,'강낭콩','2025-10-10',3,NULL,'growing','2025-11-30 16:20:02','2025-11-30 16:20:02'),(4,'leenayeon',2,1,'가지','2025-08-20',5,NULL,'growing','2025-11-30 16:20:02','2025-11-30 16:20:02');
+INSERT INTO `my_crop` VALUES (1,'leenayeon',1,115,'감자','2025-10-10',5,NULL,'growing','2025-12-01 14:17:24','2025-12-01 14:18:12'),(2,'leenayeon',2,117,'고구마','2025-10-10',5,NULL,'growing','2025-12-01 14:17:24','2025-12-01 14:18:12'),(3,'leeseyeon',3,165,'무','2025-10-15',4,NULL,'growing','2025-12-01 14:21:06','2025-12-01 14:22:24'),(5,'leeseyeon',4,115,'감자','2025-10-15',4,NULL,'growing','2025-12-01 14:22:24','2025-12-01 14:22:24'),(6,'chohyein',1,115,'감자','2025-10-15',5,NULL,'growing','2025-12-01 14:24:22','2025-12-01 14:24:22'),(7,'chohyein',2,117,'고구마','2025-10-15',5,NULL,'growing','2025-12-01 14:24:22','2025-12-01 14:24:22'),(8,'leenayeon',1,153,'고추','2025-12-03',0,NULL,'growing','2025-12-02 18:04:21','2025-12-02 18:04:21'),(9,'leenayeon',1,204,'호박','2025-12-03',0,NULL,'growing','2025-12-02 18:05:30','2025-12-02 18:05:30');
 /*!40000 ALTER TABLE `my_crop` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -63,4 +65,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-01  1:20:41
+-- Dump completed on 2025-12-03 12:12:32
